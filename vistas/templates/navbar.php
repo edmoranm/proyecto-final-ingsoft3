31e675c2a7d06ee974303a85b6f5d27fa881f9b1
edmoranm <edanielmoram@gmail.com>
2024-06-07 22:59:44 -0600

    en el navbar agregamos materias y notas

--- vistas/templates/navbar.php
+++ vistas/templates/navbar.php
@@ -18,6 +18,24 @@
             <li><a class="dropdown-item" href="../alumnos/buscar.php"><i class="bi bi-search me-2"></i>BUSCAR</a></li>
           </ul>
         </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-file-person me-2"></i>MATERIAS
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#"><i class="bi bi-plus-circle me-2"></i>CREAR</a></li>
+            <li><a class="dropdown-item" href="#"><i class="bi bi-search me-2"></i>BUSCAR</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-file-person me-2"></i>NOTAS
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#"><i class="bi bi-plus-circle me-2"></i>CREAR</a></li>
+            <li><a class="dropdown-item" href="#"><i class="bi bi-search me-2"></i>BUSCAR</a></li>
+          </ul>
+        </li>
     </div>
   </div>
 </nav>
