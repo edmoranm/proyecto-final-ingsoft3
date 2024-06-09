d006a9f7fbd158a5aa17be0c1bee49ae93416754
edmoranm <edanielmoram@gmail.com>
2024-06-09 13:02:35 -0600

    creamos el buscar en controladores notas

---
+++ controladores/notas/buscar.php
@@ -0,0 +1,120 @@
+<?php
+require_once '../../modelos/notas.php';
+require_once '../../modelos/alumnos.php';
+try {
+
+    if(isset($_GET['not_alumno']) && $_GET['not_alumno'] != ''){
+
+        $alum_id = $_GET['not_alumno'];
+
+        $alumno = new Alumno(["alum_id" => $alum_id]);
+        $alumnos = $alumno->buscar();
+
+        $Notas = new Notas(["not_alumno" => $alum_id]);
+        $Nota = $Notas->buscar();
+
+        $promedio = $Notas->promedio();     
+
+    }
+    
+
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2){
+    $error = $e2->getMessage();
+}
+
+?>
+<?php include_once '../../vistas/templates/header.php'?>
+
+
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <table class="table table-bordered table-hover">
+                    <thead class="table-dark">                    
+                        <tr>
+                            <h1 class="text-center" style="background-color: black; color: white;">NOTAS DEL ALUMNO</h1>
+                        </tr>
+                    </thead>
+                    <?php if(count($alumnos) > 0):?>
+                        <?php foreach($alumnos as $key => $alumno) : ?>
+                        <tr class="text-center">
+                        <tr>
+                            <th>ALUMNO</th>
+                            <td><?= $alumno['alum_nombre'] . ' ' . $alumno['alum_apellido']?></td>
+                        </tr>  
+                        <tr>
+                        <th>GRADO Y ARMA</th>
+                            <td><?= $alumno['alum_grado'] . ' ' . 'de' . ' ' . $alumno['alum_arma']?></td>
+                        </tr> 
+                        <tr>
+                        <th>NACIONALIDAD</th>
+                        <td><?= $alumno['alum_nacionalidad']?></td>
+                        </tr>
+                     <tbody>
+                           
+                        </tr>
+                        <?php endforeach ?>
+                        <?php else :?>
+                            <tr>
+                                <td colspan="4">NO EXISTEN REGISTROS</td>
+                            </tr>
+                        <?php endif?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <tr>
+            <h3 class="text-center" style="margin-top: 0; margin-bottom: 0;">NOTAS OBTENIDAS</h3>
+        </tr>
+        <div class="row justify-content-center mt-4">
+            <div class="col-lg-8">
+                <table class="table table-bordered table-hover">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>NO. </th>
+                            <th>MATERIA</th>
+                            <th>PUNTEO</th>
+                            <th>GANO/PERDIO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if(count($Nota) > 0):?>
+                        <?php foreach($Nota as $key => $Notas) : ?>
+                        <tr class="text-center">
+                            <td><?= $key + 1 ?></td>
+                            <td><?= $Notas['not_materia']?></td>
+                            <td><?= $Notas['not_puntos']?> PUNTOS</td>
+                            <td><?= $Notas['not_resultado']?></td>
+                        </tr>
+                        <?php endforeach ?>
+                        <?php else :?>
+                            <tr>
+                                <td colspan="4">NO EXISTEN REGISTROS</td>
+                            </tr>
+                        <?php endif?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row justify-content-center mt-4">
+            <div class="col-lg-8">
+                <table class="table table-bordered table-hover">
+                    <tbody class="text-center">
+                        <tr>
+                            <td>PROMEDIO</td>
+                            <td><?= number_format($promedio[0]['promedio'], 2, '.', ',')?> PUNTOS</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-4">
+                <a href="../../vistas/notas/buscar.php" class="btn btn-info w-100">Volver al formulario</a>
+            </div>
+        </div>
+    </div>
+    <?php include_once '../../vistas/templates/footer.php'?>
