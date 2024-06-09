13f412d4982b72abb8bd88a7334cfaf8e817c07c
edmoranm <edanielmoram@gmail.com>
2024-06-08 20:38:41 -0600

    se  creo el controlador de buscar en materias

---
+++ controladores/materias/buscar.php
@@ -0,0 +1,85 @@
+<?php
+    //  ini_set('display_errors', '1');
+    //  ini_set('display_startup_errors', '1');
+    //  error_reporting(E_ALL);
+    require '../../modelos/materias.php';
+
+    // consulta
+    try {
+        // var_dump($_GET);
+        $_GET['mat_nobmre'] = htmlspecialchars( $_GET['mat_nobmre']);
+    
+        $objMateria = new Materia($_GET);
+        $materia = $objMateria->buscar();
+        $resultado = [
+            'mensaje' => 'Datos encontrados',
+            'datos' => $materia,
+            'codigo' => 1
+        ];
+
+    } catch (Exception $e) {
+        $resultado = [
+            'mensaje' => 'OCURRIO UN ERROR EN LA EJECUCIÓN',
+            'detalle' => $e->getMessage(),
+            'codigo' => 0
+        ];
+    }       
+
+
+    $alertas = ['danger', 'success', 'warning'];
+
+    
+    include_once '../../vistas/templates/header.php'; ?>
+
+    <div class="row mb-4 justify-content-center">
+        <div class="col-lg-6 alert alert-<?=$alertas[$resultado['codigo']] ?>" role="alert">
+            <?= $resultado['mensaje'] ?>
+        </div>
+    </div>
+    <div class="row mb-4 justify-content-center">
+        <div class="col-lg-6">
+            <a href="../../vistas/materias/buscar.php" class="btn btn-primary w-100">Volver al formulario de busqueda</a>
+        </div>
+    </div>
+    <h1 class="text-center">Listado de materias</h1>
+    <div class="row justify-content-center">
+        <div class="col-lg-10">
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Nombre</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if($resultado['codigo'] == 1 && count($materias) > 0) : ?>
+                        <?php foreach ($materias as $key => $materia) : ?>
+                            <tr>
+                                <td><?= $key + 1?></td>
+                                <td><?= $materia['mat_nombre'] ?></td>
+                                <td class="text-center">
+                                <div class="dropdown">
+                                    <button class="btn btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Acciones
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="../../vistas/materias/modificar.php?mat_id=<?= base64_encode($materia['mat_id'])?>"><i class="bi bi-pencil-square me-2"></i>Modificar</a></li>
+                                        <li><a class="dropdown-item" href="../../controladores/materias/eliminar.php?mat_id=<?= base64_encode($materia['mat_id'])?>"><i class="bi bi-trash me-2"></i>Eliminar</a></li>
+                                    </ul>
+                                </div>
+
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                    <?php else : ?>
+                        <tr>
+                            <td colspan="4">No hay materias registrados</td>
+                        </tr>  
+                    <?php endif ?>
+                </tbody>
+                        
+            </table>
+        </div>        
+    </div>        
+<?php include_once '../../vistas/templates/footer.php'; ?>  
